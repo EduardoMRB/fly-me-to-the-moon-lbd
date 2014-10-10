23f7266100b7fff7b67f49ffd8389e97e83c81bc
Eduardo Borges <eduardomrb@gmail.com>
2014-10-10 00:09:40 -0400

    Add companies and engineers sql schemas

---
+++ schema/companies.sql
@@ -0,0 +1,5 @@
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  document integer NOT NULL
+);
---
+++ schema/engineers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE engineers (
+  id SERIAL PRIMARY KEY,
+  company_id integer NOT NULL,
+  name varchar(50) NOT NULL,
+  birth_date date NOT NULL,
+
+  FOREIGN KEY (company_id) REFERENCES companies (id) ON DELETE CASCADE
+);
